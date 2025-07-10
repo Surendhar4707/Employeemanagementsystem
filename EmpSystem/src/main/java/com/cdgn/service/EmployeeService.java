cca5f881b0fc16a1d6e7c4d6198806f55e576534
Surendhar4707 <sayenasurendhar@gmail.com>
2025-07-10 13:58:15 +0530

    version 1

---
+++ EmpSystem/src/main/java/com/cdgn/service/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.cdgn.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cdgn.model.Employee;
+import com.cdgn.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository repository;
+
+	public Employee saveEmployee(Employee employee) {
+		return repository.save(employee);
+	}
+
+	public boolean existsByEmailAndPassword(String email, String password) {
+		return repository.existsByEmailAndPassword(email,password);
+	}
+
+	public List<Employee> viewAllEmployees() {
+		return repository.findAll();
+	}
+
+	public Employee findByEmail(String email) {
+		return repository.findByEmail(email);
+	}
+
+	public void deleteById(int id) {
+		repository.deleteById(id);
+		
+	}
+
+}
